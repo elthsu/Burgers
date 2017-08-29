7c32d65b2c89457b9c41950c72e9323601e8505e
Elton Hsu <eltonhsu@MacBook-Pro-3.local>
2017-08-29 10:07:21 -0700

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Cheeseburger with onions', true, true, true, false);
-INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken with Cheese, no onions', false, false, true, false);
-INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Hamburger with no onions', true, false, false, false);
-INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken with onions, no cheese', false, true, true, false);
-INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('BigMac with onions and cheese', true, true, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Cheeseburger', true, true, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken', false, false, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Hamburger', true, false, false, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken', false, true, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('BigMac', true, true, true, false);
