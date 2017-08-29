4f6a3e113cd2376900a4c324c988bf56033c61cb
Elton Hsu <eltonhsu@MacBook-Pro-3.local>
2017-08-28 18:28:29 -0700

    completed

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+### Schema
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	meat BOOLEAN NOT NULL DEFAULT true,
+	onion BOOLEAN NOT NULL DEFAULT true,
+	cheese BOOLEAN NOT NULL DEFAULT true,
+	devoured BOOLEAN NOT NULL DEFAULT false,
+	date TIMESTAMP,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Cheeseburger with onions', true, true, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken with Cheese, no onions', false, false, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Hamburger with no onions', true, false, false, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken with onions, no cheese', false, true, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('BigMac with onions and cheese', true, true, true, false);
