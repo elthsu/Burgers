6d869fdd65b901ba31aea33f542c0702458a7024
Elton Hsu <eltonhsu@MacBook-Pro-3.local>
2017-08-28 18:49:00 -0700

    db update

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burger_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burger_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
